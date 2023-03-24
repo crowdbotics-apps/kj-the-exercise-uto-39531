a300830e1a58d948b203a7df6cfc1ac7c1fb67ec
Crowdbotics <team@crowdbotics.com>
2023-03-24 07:27:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kj-the-exercise-uto-39531",
+  url: "https://kj-the-exercise-uto-39531.botics.co",
+  api: "https://kj-the-exercise-uto-39531.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
